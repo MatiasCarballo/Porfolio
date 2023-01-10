f27553d71eb7712e4e2ed15b89fb78b12d9644dc
Matias Carballo <matiascarballo03@hotmail.com>
2023-01-10 16:10:56 -0300

    new NavBar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './Sass/style.scss'
+import NavBar from "./Components/NavBar";
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
